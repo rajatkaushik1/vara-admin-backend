d8bf25be74c23faced0ed44f769a675095f9483d
rajat <rajatrajkaushik1@gmail.com>
2025-07-19 14:13:59 +0530

    Update Song.js

--- models/Song.js
+++ models/Song.js
@@ -2,46 +2,39 @@
 const mongoose = require('mongoose');
 
 const songSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  imageUrl: {
-    type: String,
-    required: true
-  },
-  audioUrl: {
-    type: String,
-    required: true
-  },
-  genres: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Genre',
-    required: true
-  }],
-  subGenres: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'SubGenre',
-    required: true
-  }],
-  // REMOVED: isExclusive field as per new requirements
-  // isExclusive: {
-  //   type: Boolean,
-  //   default: false
-  // },
-  collectionType: {
-    type: String,
-    required: true,
-    // CHANGED: Enum values to only 'free' and 'paid'
-    enum: ['free', 'paid']
-  },
-  // You can add an 'artist' field here if it's missing and used in your controller
-  // artist: {
-  //   type: String,
-  //   required: true, // or false, depending on your requirements
-  //   trim: true
-  // },
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    audioUrl: {
+        type: String,
+        required: true
+    },
+    // NEW FIELD: Added duration here
+    duration: {
+        type: Number, // Store duration in seconds
+        default: 0,   // Default to 0 if not provided, will be updated on upload
+    },
+    genres: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Genre',
+        required: true
+    }],
+    subGenres: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'SubGenre',
+        required: true
+    }],
+    collectionType: {
+        type: String,
+        required: true,
+        enum: ['free', 'paid']
+    },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Song', songSchema);
